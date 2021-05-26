0cf8762ce5ac40d7087e18dd92547700dc053c7f
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-26 17:59:39 +0200

    Update completata

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,9 +5,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Tag;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+
+    protected $validateFields = [];
+
+    public function __construct() {
+        $this->validateFields = [
+            'title' => 'required | string | unique:posts',
+            'image' => 'nullable | url',
+            'author' => 'required | string',
+            'content' => 'required | string',
+            'date' => 'required | string'
+        ];
+    } 
+
     /**
      * Display a listing of the resource.
      *
@@ -70,9 +84,24 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $validation = $this->validateFields;
+        $validation['title'] .= ',title,' . $post->id;
+
+        $request->validate($validation);
+
+        $data = $request->all();
+
+        $data['public'] = !isset($data['public']) ? 0 : 1;
+
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        $post->update($data);
+
+        $post->tags()->sync($data['tags']);
+
+        return redirect()->route('admin.posts.show', $post);
     }
 
     /**
